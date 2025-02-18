582b1633d6e8fb69c2b1412362fc06b30c6ef6f1
martin <martinrombach88@gmail.com>
2025-02-18 12:08:30 +0900

    base script

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: mode === "production" ? "/martinrombach88.github.io/" : "/", // Replace with your repo name
+  base: "/martinrombach88.github.io/",
 });
