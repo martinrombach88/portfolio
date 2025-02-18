363723ea38b698e34df694f7da9902799f2e4776
martin <martinrombach88@gmail.com>
2025-02-18 11:49:47 +0900

    deployment fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: mode === "production" ? "/" : "/martinrombach88.github.io/", // Replace with your repo name
+});
