b661bd4645dcfb399c00731d35518db6a578c0ca
martin <martinrombach88@gmail.com>
2025-02-18 16:48:46 +0900

    bootstrap css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+//import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 
